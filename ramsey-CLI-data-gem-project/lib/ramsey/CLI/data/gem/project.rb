91c653d8bb4802e450f0134d218aaf0283ca8c6a
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-15 22:26:23 +0000

    modified readme

---
+++ ramsey-CLI-data-gem-project/lib/ramsey/CLI/data/gem/project.rb
@@ -0,0 +1,14 @@
+require "ramsey/CLI/data/gem/project/version"
+
+module Ramsey
+  module CLI
+    module Data
+      module Gem
+        module Project
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ ramsey-CLI-data-gem-project/lib/ramsey/CLI/data/gem/project/version.rb
@@ -0,0 +1,11 @@
+module Ramsey
+  module CLI
+    module Data
+      module Gem
+        module Project
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
